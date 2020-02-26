870db7fd5f460096f3803198666f284ac65275a0
Alex <afatyga@bu.edu>
2020-02-26 00:40:37 -0500

    we are getting there!!!

--- test_tweets.py
+++ test_tweets.py
@@ -4,6 +4,12 @@ import twitterHW2
 def test_compare(): #these tests test the threading system set up but cannot test the multiprocessing set up
 	assert twitterHW2.startUp(['BU_tweets',0]) == 1
 	assert twitterHW2.getMsgs(123) == []
-	assert twitterHW2.startUp(["thegoldenratio4", 1]) ==1
+	assert twitterHW2.startUp(["thegoldenratio4", 1]) == 1
+	assert twitterHW2.startUp(["dog_rates", 3]) == 1
+	assert twitterHW2.startUp(["onlyhiphopfacts", 6]) == 1
+	assert twitterHW2.startUp(["busasquatch", 0]) == 1
+	assert twitterHW2.startUp(["johnmulaneybot", 12]) == 1
+	assert twitterHW2.startUp(["mileycyrus", 8]) == 1
+
 
 #to test the multiprocessing set up (hw3queue.py), modify the restartProgram.bat or restartProgram.sh - details in github readme
