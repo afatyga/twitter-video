988059798aeee03c1bac2990f5dca7c222f42f7a
Alex <afatyga@bu.edu>
2020-02-25 22:28:00 -0500

    i made so many mistakes

--- test_tweets.py
+++ test_tweets.py
@@ -2,9 +2,9 @@ import twitterHW2
 #can only test certain tests on local
 
 def test_compare(): #these tests test the threading system set up but cannot test the multiprocessing set up
-	assert twitterHW2.startUp(['a'], 0) == 0
+	assert twitterHW2.startUp(['a', 0]) == 0
 	assert twitterHW2.startUp(['BU_tweets',0]) == 1
-	assert twitterHW2.getMsgs(123) == 0 
-	assert twitterHW2.getMsgs("fakefakefakeusername1234211") == 0
+	assert twitterHW2.getMsgs([123,0]) == 0 
+	assert twitterHW2.getMsgs(["fakefakefakeusername1234211",0]) == 0
 
 #to test the multiprocessing set up (hw3queue.py), modify the restartProgram.bat or restartProgram.sh - details in github readme
