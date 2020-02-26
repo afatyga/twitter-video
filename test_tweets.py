ffbe520fa8b7bed441523dd756c7f3942a51b0cf
Alex <afatyga@bu.edu>
2020-02-25 22:18:36 -0500

    fixes

--- test_tweets.py
+++ test_tweets.py
@@ -1,8 +1,8 @@
-#import twitterHW2
+import twitterHW2
 #can only test certain tests on local
 
 def test_compare(): #these tests test the threading system set up but cannot test the multiprocessing set up
-	assert twitterHW2.startUp([a], 0) == 0
+	assert twitterHW2.startUp(['a'], 0) == 0
 	assert twitterHW2.startUp(['BU_tweets',0]) == 1
 	assert twitterHW2.getMsgs(123) == [] 
 	assert twitterHW2.getMsgs("fakefakefakeusername1234211") == []
