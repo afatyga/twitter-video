9e2b38b7cab0c7a96c7374713c4b73360514d6a0
Alex <afatyga@bu.edu>
2020-02-25 22:23:26 -0500

    even more fixes! woo

--- test_tweets.py
+++ test_tweets.py
@@ -5,6 +5,6 @@ def test_compare(): #these tests test the threading system set up but cannot tes
 	assert twitterHW2.startUp(['a'], 0) == 0
 	assert twitterHW2.startUp(['BU_tweets',0]) == 1
 	assert twitterHW2.getMsgs(123) == 0 
-	assert twitterHW2.getMsgs("fakefakefakeusername1234211") == 1
+	assert twitterHW2.getMsgs("fakefakefakeusername1234211") == 0
 
 #to test the multiprocessing set up (hw3queue.py), modify the restartProgram.bat or restartProgram.sh - details in github readme
